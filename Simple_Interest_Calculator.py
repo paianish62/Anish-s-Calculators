ff5b6b03cc3cec956ce1fc7b73ee5edc5f06426d
paianish62 <69522714+paianish62@users.noreply.github.com>
2021-01-30 09:46:58 +0400

    Update Simple_Interest_Calculator.py

--- Simple_Interest_Calculator.py
+++ Simple_Interest_Calculator.py
@@ -1,6 +1,6 @@
-x = int(input())
+x = int(input("Enter Principal"))
 '''rate = 5%per year , for 3 years'''
-z = int(input())
-k = int(input())
+z = int(input("Enter Rate"))
+k = int(input("Enter Time"))
 print((x*z*k)/100)
-docs.rerout(     
+     
